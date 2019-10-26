84ce05f1e12e1a2901b9a7349178b683a371c126
moiz <muhammadmoizshaikh@gmail.com>
2019-10-26 12:33:27 +0500

    fixes

--- src/Layout/AppMain/index.js
+++ src/Layout/AppMain/index.js
@@ -26,7 +26,7 @@ const AppMain = () => {
 
             <Suspense fallback={
                 <div className="loader-container">
-                    <div className="loader-container-inner">
+                    {/* <div className="loader-container-inner">
                         <div className="text-center">
                             <Loader type="ball-pulse-rise"/>
                         </div>
@@ -34,7 +34,7 @@ const AppMain = () => {
                             Please wait while we load all the Components examples
                             <small>Because this is a demonstration we load at once all the Components examples. This wouldn't happen in a real live app!</small>
                         </h6>
-                    </div>
+                    </div> */}
                 </div>
             }>
                 <Route path="/components" component={Components}/>
